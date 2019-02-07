518bab9d16cdae51ef5a709b298eb6bdb5a0e14f
lhh8048 <lhh8048@naver.com>
2019-02-07 11:55:07 +0900

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,9 +1,5 @@
-#include <iostream>
-
-using namespaces std;
-
-int main()
-{
-  cout<<"hello \n";
-  return 0;
-  }
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
